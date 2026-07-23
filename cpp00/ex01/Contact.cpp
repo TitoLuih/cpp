8109f76a0e3f3ca35271bf2e000611456350c0c3
TitoLuih <luisruizmalaga@gmail.com>
2026-07-23 18:12:42 +0200

    changes made

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -55,3 +55,14 @@ void Contact::setDarkestSecret(const std::string& darkestSecret)
 {
 	this->_darkestSecret = darkestSecret;
 }
+
+void Contact::displayContactInfo() const 
+{
+	std::cout << "--- Contacts details Info ---" << std::endl;
+    std::cout << "First name:    " << _firstName << std::endl;
+    std::cout << "Last name:     " << _lastName << std::endl;
+    std::cout << "Nickname:      " << _nickName << std::endl;
+	std::cout << "Phone Number:  " << _phoneNumber << std::endl;
+	std::cout << "Secret:        " << _darkestSecret << std::endl;
+    std::cout << "-----------------------------" << std::endl;
+}
